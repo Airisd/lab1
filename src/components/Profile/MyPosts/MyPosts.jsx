0a787b842eabff220b8cc47a04835eb4a8384fa6
Dmytro Usatiuk <bows.usages_0w@icloud.com>
2023-02-18 18:51:44 +0200

    15 lesson

---
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import s from "./MyPosts.module.css";
+
+const MyPosts = () => {
+  return (
+    <div>
+      My Posts
+      <div>New post</div>
+      <div className={s.posts}>
+        <div className={s.item}>post 1</div>
+        <div className="item">post 2</div>
+      </div>
+    </div>
+  );
+};
+
+export default MyPosts;
